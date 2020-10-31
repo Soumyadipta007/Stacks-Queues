ca82c6ba4408243ea3e07059a0a0b359216ba45e
Soumya <soumyadipta0077@gmail.com>
2020-10-31 18:57:17 +0530

    [Soumya] Add: Method added to add data to stack

---
+++ Stack-Queue/Stack-Queue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stack_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackUsingLL stackUsingLL = new StackUsingLL();
+            stackUsingLL.Push(70);
+            stackUsingLL.Push(30);
+            stackUsingLL.Push(56);
+            Console.WriteLine("Stack Structure- ");
+            stackUsingLL.Display();
+        }
+    }
+}
